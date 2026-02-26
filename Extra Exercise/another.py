6972af8861c70903c91e63e23b8531d53243d1be
Omkar Bhattarai <omkarbhattarai71@gmail.com>
2026-02-26 12:11:53 +0100

    Tested the functionality of main in Python

---
+++ Extra Exercise/another.py
@@ -0,0 +1,5 @@
+import test_main  # This will print "This ALWAYS runs - even when imported!"
+             # But NOT "This ONLY runs when executed directly"
+
+result = test_main.some_function()
+print(result)
---
+++ Extra Exercise/test_main.py
@@ -0,0 +1,7 @@
+def some_function():
+    return "Hello"
+
+print("This ALWAYS runs - even when imported!")
+
+if __name__ == "__main__":
+    print("This ONLY runs when executed directly")
